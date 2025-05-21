e53f05b19c210e5de7560d8fae1e526338262d8f
Mnaim03 <99801017+Mnaim03@users.noreply.github.com>
2025-05-21 15:44:05 +0200

    refactor e codice

---
+++ raspVHF/RunArduino.py
@@ -0,0 +1,19 @@
+import subprocess
+
+sketch_path = "/Documents/RaspVHF/raspVHF/arduino/main.ino"
+fqbn = "arduino:avr:uno"
+serial_port = "/dev/ttyACM0"
+
+# Compilazione
+try:
+    subprocess.run(["arduino-cli", "compile", "--fqbn", fqbn, sketch_path], check=True)
+    print("Compilazione riuscita")
+except subprocess.CalledProcessError:
+    print("Errore nella compilazione")
+
+# Upload
+try:
+    subprocess.run(["arduino-cli", "upload", "-p", serial_port, "--fqbn", fqbn, sketch_path], check=True)
+    print("Upload riuscito")
+except subprocess.CalledProcessError:
+    print("Errore nell'upload")
