d6d636f772d6ad20b3eb6db0f667db73e1fe6849
Mnaim03 <99801017+Mnaim03@users.noreply.github.com>
2025-05-21 16:49:47 +0200

    Update run.sh

--- raspVHF/run.sh
+++ raspVHF/run.sh
@@ -2,11 +2,13 @@ cd /home/user1/Documents/RaspVHF/raspVHF
 git restore . #elimino eventuali cambiamenti fatti dal terminale raspberry
 git pull #faccio una pull di aggiornamento
 
+clear #pulisco terminale
+
 #do i permessi ai file
 chmod +x CompileArduino.py
 chmod +x loopSimulator.py
 chmod +x arduino/arduino.ino
 
 #avvio file python
-python3 RunArduino.py
+python3 CompileArduino.py
 python3 loopSimulator.py
