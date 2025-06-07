35553d969913b1b97f33e2bac6f12877a1f2e890
Mnaim03 <99801017+Mnaim03@users.noreply.github.com>
2025-06-07 12:18:52 +0200

    Update mainHandler.py

--- raspVHF/mainHandler.py
+++ raspVHF/mainHandler.py
@@ -84,8 +84,8 @@ def start_Arduino():
 def update_arduino(arduino):
     # 1 -> No Anomalia
     # 2 -> Anomalia
-    if get_anomalia() == False : flag = 1
-    else: flag = 2
+    if get_anomalia() == 1 : flag = 2
+    else: flag = 1
 
     arduino.write(f"{flag}\n".encode())
     # stampa seriale
