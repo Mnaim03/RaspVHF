cc12de6ac7acacfd8c1b6904301a266bd12e6a66
Mnaim03 <99801017+Mnaim03@users.noreply.github.com>
2025-06-12 10:02:19 +0200

    Update checkHandler.py

--- raspVHF/checkHandler.py
+++ raspVHF/checkHandler.py
@@ -8,7 +8,7 @@ class OldInput:
         self.oldFrequence = get_frequence_num()
         self.oldHz = get_frequence_hz()
         self.oldAnomalia = get_anomalia()
-        return
+        
 
     def checkOld(self):
         if (self.oldAnomalia != get_anomalia()) or (self.oldFrequence != get_frequence_num()) or (self.oldHz != get_frequence_hz()):
