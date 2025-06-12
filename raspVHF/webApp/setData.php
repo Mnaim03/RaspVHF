0c51a6b64677fd8403f3b37c3b8e176cabfa391b
Mnaim03 <99801017+Mnaim03@users.noreply.github.com>
2025-06-12 10:44:53 +0200

    Update setData.php

--- raspVHF/webApp/setData.php
+++ raspVHF/webApp/setData.php
@@ -5,22 +5,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $filename = "Data";
 
-    // Costruisci il contenuto da salvare
-    $contenuto = "frequence_num = $num\nfreuqnece_hz = $hz\n";
-
-    // Sovrascrivi completamente il file
-    $existingData = file_exists($filename) ? file_get_contents($filename) : "";
-    parse_str(str_replace(["\n", " = "], ["&", "="], $existingData), $data);
-    $data['frequence_num'] = $num;
-    $data['freuqnece_hz'] = $hz;
-    $newContent = "";
-    foreach ($data as $key => $value) {
-        $newContent .= "$key = $value\n";
+    if (file_exists($filename)) {
+        $lines = file($filename, FILE_IGNORE_NEW_LINES);
+        foreach ($lines as &$line) {
+            if (str_starts_with($line, 'frequence_num =')) {
+                $line = "frequence_num = $num";
+            } elseif (str_starts_with($line, 'freuqnece_hz =')) {
+                $line = "freuqnece_hz = $hz";
+            }
+        }
+        file_put_contents($filename, implode("\n", $lines));
+    } else {
+        $lines = [
+            "frequence_num = $num",
+            "freuqnece_hz = $hz"
+        ];
+        file_put_contents($filename, implode("\n", $lines));
     }
-    file_put_contents($filename, $newContent);
 
     echo "<p>✅ File aggiornato con successo!</p>";
-    echo "<pre>$contenuto</pre>";
+    echo "<pre>frequence_num = $num\nfreuqnece_hz = $hz</pre>";
     echo '<a href="index.html">Torna alla pagina principale</a>';
 } else {
     header("Location: index.html");
