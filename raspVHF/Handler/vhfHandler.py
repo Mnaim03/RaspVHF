28c4532d8c64bcb09294200884cd480d1fa29e2c
Mnaim03 <99801017+Mnaim03@users.noreply.github.com>
2025-06-18 10:37:02 +0200

    Update vhfHandler.py

--- raspVHF/Handler/vhfHandler.py
+++ raspVHF/Handler/vhfHandler.py
@@ -28,7 +28,7 @@ def set_freuqneza_sdr(sdr):
     input_hz = unit_to_multiplier(input_unit)
     sdr.center_freq = int(input_freq * input_hz)
 
-    sdr.gain = 'auto'
+    sdr.gain = 1
     sdr.sample_rate = 2.4 * input_hz
 
 def stampa_ascii_spectrum(freqs, power, threshold):
