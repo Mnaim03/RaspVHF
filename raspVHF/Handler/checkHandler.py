de231d333ffb79fb0ef331c75281646210a99802
Mnaim03 <99801017+Mnaim03@users.noreply.github.com>
2025-06-19 10:57:25 +0200

    Update checkHandler.py

--- raspVHF/Handler/checkHandler.py
+++ raspVHF/Handler/checkHandler.py
@@ -4,12 +4,12 @@ class lastInput:
     def __init__(self):
         self.lastFrequence = int(get_frequence_num())
         self.lastHz = str(get_frequence_hz())
-        self.lastAnomalia = bool(get_anomalia())
+        self.lastAnomalia = get_anomalia()
 
     def checkChange(self):
         print(f"[DEBUG] Anomalia: Last={self.lastAnomalia}, Now={get_anomalia()}")
         if (
-            self.lastAnomalia != bool(get_anomalia()) or
+            self.lastAnomalia != get_anomalia() or
             self.lastFrequence != int(get_frequence_num()) or
             self.lastHz != str(get_frequence_hz())
         ):
