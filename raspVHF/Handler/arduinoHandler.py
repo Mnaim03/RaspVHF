7bebcebb76ad05b64464346cf491b9d47ae7ed48
Mnaim03 <99801017+Mnaim03@users.noreply.github.com>
2025-06-19 11:13:10 +0200

    Update arduinoHandler.py

--- raspVHF/Handler/arduinoHandler.py
+++ raspVHF/Handler/arduinoHandler.py
@@ -17,8 +17,10 @@ def start_Arduino():
 def update_arduino(arduino):
     # 1 -> No Anomalia
     # 2 -> Anomalia
-    if get_anomalia() : flag = 1
-    else: flag = 2
+    if get_anomalia() :
+        flag = 1
+    else:
+        flag = 2
 
     print(f"{get_anomalia()} | {flag}")
 
